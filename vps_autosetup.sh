f1710d719c6f39d3bac3bf1ae57fad3904d1e0dd
vrzdrb <amhdws@protonmail.com>
2025-12-03 00:02:32 +0500

    Не задалось с цветным оформлением, убрал

--- vps_autosetup.sh
+++ vps_autosetup.sh
@@ -2,11 +2,8 @@
 
 # VPS autosetup
 
-PINK='\033[1;95m'
-NC='\033[0m'
-
 if [ "$(id -u)" != "0" ]; then
-   echo "${PINK}### Этот скрипт должен быть запущен от root ###${NC}" 1>&2
+   echo "### Этот скрипт должен быть запущен от root ###" 1>&2
    exit 1
 fi                                                                                          
 apt update
@@ -15,12 +12,12 @@ apt install mc git curl wget iptables-persistent chkrootkit rkhunter rsyslog ran
 
 # Включаем unattended-upgrades
 
-echo "${PINK}### Включение Unattended Upgrades ###${NC}"
+echo "### Включение Unattended Upgrades ###"
 apt update && apt install -y unattended-upgrades
 echo 'APT::Periodic::Update-Package-Lists "1";
 APT::Periodic::Unattended-Upgrade "1";' > /etc/apt/apt.conf.d/20auto-upgrades
 sed -i 's|//\s*"${distro_id}:${distro_codename}-security";|"${distro_id}:${distro_codename}-security";|' /etc/apt/apt.conf.d/50unattended-upgrades
-echo "${PINK}### Автообновления включены ###${NC}"
+echo "### Автообновления включены ###"
 
 # Включаем BBR, отключаем IPv6
                                                                           
@@ -32,13 +29,13 @@ net.ipv6.conf.default.disable_ipv6 = 1
 net.ipv6.conf.lo.disable_ipv6 = 1
 " >> /etc/sysctl.conf'
 sysctl -p
-echo "${PINK}### Если вы видите ошибку, откройте /etc/sysctl.conf и измените eth0 на ваш интерфейс из ifconfig ###${NC}"
+echo "### Если вы видите ошибку, откройте /etc/sysctl.conf и измените eth0 на ваш интерфейс из ifconfig ###"
 read -r
 
 # Включаем автоматическую перезагрузку в 4:20 утра каждый понедельник
 
 (crontab -l 2>/dev/null; echo "20 4 * * 1 /sbin/reboot") | crontab -
-echo "${PINK}### Добавлена перезагрузка в 4:20 каждый понедельник ###${NC}"
+echo "### Добавлена перезагрузка в 4:20 каждый понедельник ###"
 
 # Устанавливаем и настраиваем Fail2Ban
 
@@ -53,67 +50,67 @@ maxretry = 5
 enabled = true
 EOF
 systemctl enable fail2ban && systemctl start fail2ban
-echo "${PINK}### fail2ban настроен: бан на 12 часов спустя 5 попыток за 15 минут ###${NC}"
+echo "### fail2ban настроен: бан на 12 часов спустя 5 попыток за 15 минут ###"
 
 # Добавляем пользователя sudo
                                                                    
-read -p "${PINK}### Введите имя пользователя: ###${NC}" username
+read -p "### Введите имя пользователя: ###" username
 adduser --gecos "" $username
 usermod -aG sudo $username
-echo "${PINK}### Пользователь $username создан и добавлен в sudo ###${NC}"
+echo "### Пользователь $username создан и добавлен в sudo ###"
 
 # Включаем логгирование sudo
                                         
-echo "${PINK}### В следующем файле добавьте строку 'Defaults logfile=/var/log/sudo.log, log_input, log_output'"
-read -r -p "${PINK}### Окей, я скопировал её и готов ###${NC}"
+echo "### В следующем файле добавьте строку 'Defaults logfile=/var/log/sudo.log, log_input, log_output'"
+read -r -p "### Окей, я скопировал её и готов ###"
 sudo visudo
 
 # Для AmneziaVPN self-hosted
 
-read -p "${PINK}### Вы разворачиваете AmneziaVPN self-hosted? Y/n: ###${NC}" -r
+read -p "### Вы разворачиваете AmneziaVPN self-hosted? Y/n: ###" -r
 echo
 
 if [[ $REPLY =~ ^[Yy]$ ]]; then
     echo "$username ALL=(ALL) NOPASSWD: ALL" | sudo EDITOR='tee' visudo -f /etc/sudoers.d/$username-nopasswd
-    echo "${PINK}### sudo-операции для $username НЕ БУДУТ ТРЕБОВАТЬ ПАРОЛЬ ###${NC}"
+    echo "### sudo-операции для $username НЕ БУДУТ ТРЕБОВАТЬ ПАРОЛЬ ###"
 else
-    echo "${PINK}### sudo-операции для $username будут происходить как обычно ###${NC}"
+    echo "### sudo-операции для $username будут происходить как обычно ###"
 fi
 
 # Отключаем вход через root
 
 sed -i 's/^PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config
 systemctl restart ssh
-echo "${PINK}### Вход под root отключен ###${NC}"
+echo "### Вход под root отключен ###"
 
-echo "${PINK}=======================================================================================${NC}"
-echo "${PINK}### ВНИМАТЕЛЬНО ПРОЧТИТЕ: сейчас вам нужно открыть терминал на своём компьютере ${NC}"
-echo "${PINK}### И создать ssh-ключ для входа на сервер следующей командой: ${NC}"
-echo "${PINK}### ssh-keygen -t ed25519 -C "имяключа" -f ~/.ssh/имяключа ${NC}"
-echo "${PINK}### Загрузите ключ на сервер (команда для Linux): ${NC}"
-echo "${PINK}### ssh-copy-id -i ~/.ssh/имяключа.pub логин@IP.сервера ${NC}"
-echo "${PINK}### Команда для Windows: ${NC}"
-echo "${PINK}### cat ~/.ssh/id_ed25519.pub | ssh логин@IP.сервера" mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys" ${NC}"
-echo "${PINK}### Попробуйте подключиться к вашему серверу при помощи этого ключа: ${NC}"
-echo "${PINK}### ssh -i ~/.ssh/имяключа логин@IP.сервера ${NC}"
-echo "${PINK}### ТОЛЬКО ЕСЛИ ВХОД УСПЕШЕН, нажмите здесь Enter для ОТКЛЮЧЕНИЯ ВХОДА В SSH ПО ПАРОЛЮ ${NC}"
-echo "${PINK}=======================================================================================${NC}"
+echo "======================================================================================="
+echo "### ВНИМАТЕЛЬНО ПРОЧТИТЕ: сейчас вам нужно открыть терминал на своём компьютере "
+echo "### И создать ssh-ключ для входа на сервер следующей командой: "
+echo "### ssh-keygen -t ed25519 -C "имяключа" -f ~/.ssh/имяключа "
+echo "### Загрузите ключ на сервер (команда для Linux): "
+echo "### ssh-copy-id -i ~/.ssh/имяключа.pub логин@IP.сервера "
+echo "### Команда для Windows: "
+echo "### cat ~/.ssh/id_ed25519.pub | ssh логин@IP.сервера" mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys" "
+echo "### Попробуйте подключиться к вашему серверу при помощи этого ключа: "
+echo "### ssh -i ~/.ssh/имяключа логин@IP.сервера "
+echo "### ТОЛЬКО ЕСЛИ ВХОД УСПЕШЕН, нажмите здесь Enter для ОТКЛЮЧЕНИЯ ВХОДА В SSH ПО ПАРОЛЮ "
+echo "======================================================================================="
 read -r
 grep -r PasswordAuthentication /etc/ssh -l | xargs -n 1 sed -i -e "/PasswordAuthentication /c\PasswordAuthentication no"
-echo "${PINK}### Вход по паролю отключён ###${NC}"
+echo "### Вход по паролю отключён ###"
 
 # Смена SSH-порта
 
-read -p "${PINK}### Введите новый SSH порт: ###${NC}" port
+read -p "### Введите новый SSH порт: ###" port
 sed -i "s/^#Port.*/Port $port/" /etc/ssh/sshd_config
 sed -i "s/^Port.*/Port $port/" /etc/ssh/sshd_config
 grep -q "^Port" /etc/ssh/sshd_config || echo "Port $port" >> /etc/ssh/sshd_config
 systemctl restart ssh
-echo "${PINK}### SSH порт изменен на $port ###${NC}"
+echo "### SSH порт изменен на $port ###"
 
 # Настройка iptables и блокировка IP-адресов РКН
 
-echo "${PINK}### Настройка iptables ###${NC}"
+echo "### Настройка iptables ###"
 iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -A INPUT -p tcp -m state --state NEW -m tcp --dport $port -j ACCEPT 
 iptables -A INPUT -p tcp -m tcp --dport 80 -j ACCEPT
@@ -128,13 +125,13 @@ git clone https://github.com/freemedia-tech/iptables-rugov-block.git
 cd iptables-rugov-block
 ./install.sh
 
-echo "${PINK}### Настройка iptables завершена ###${NC}"
+echo "### Настройка iptables завершена ###"
 
 # Добавляем 2 Гб подкачки для самых дешёвых VPS с 1 Гб RAM
 
 swapfile="/swapfile"
 if [ -f "$swapfile" ]; then
-    echo "${PINK}### Swap файл уже существует ###${NC}"
+    echo "### Swap файл уже существует ###"
     exit 1
 fi
 dd if=/dev/zero of=$swapfile bs=1M count=2048
@@ -142,7 +139,7 @@ chmod 600 $swapfile
 mkswap $swapfile
 swapon $swapfile
 echo "$swapfile none swap sw 0 0" >> /etc/fstab
-echo "${PINK}### Создан swap файл 2GB и активирован ###${NC}"
+echo "### Создан swap файл 2GB и активирован ###"
                            
 # Смотрим GeoIP
                 
