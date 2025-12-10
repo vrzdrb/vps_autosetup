b44fd5373a6bda37410df9088e1cce62266e35e4
vrzdrb <amhdws@protonmail.com>
2025-12-10 23:29:37 +0500

    bug fixes

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # vps_autosetup
 The script automatically performs some basic configuration for your VPS such as:
 - Enabling: unattended-upgrades, BBR, auto reboot (once a week), sudo logging, 2Gb swap; 
-- Add sudo user, create & upload ssh-key (only Linux commands), change SSH port;
+- Add sudo user, create & upload ssh-key, change SSH port;
 - Disabling: IPv6, root login, password authentication;
 - Configuring iptables (read this section carefully);
 - Show ipregion (by https://ipregion.vrnt.xyz);
@@ -11,7 +11,6 @@ The script automatically performs some basic configuration for your VPS such as:
 - Debian Linux on your VPS (or Ubuntu, but Debian is preferable)
 
 ## Download:
-
 ```bash
 curl -o vps_autosetup.sh https://raw.githubusercontent.com/vrzdrb/vps_autosetup/main/vps_autosetup.sh
 ```
--- vps_autosetup.sh
+++ vps_autosetup.sh
@@ -3,21 +3,25 @@
 # VPS autosetup
 
 if [ "$(id -u)" != "0" ]; then
-   echo "### Этот скрипт должен быть запущен от root ###" 1>&2
+   echo "Этот скрипт должен быть запущен от root" 1>&2
    exit 1
-fi                                                                                          
+fi
+
+echo "Обновление"                                                                                          
 apt update
 apt upgrade
-apt install mc git curl wget iptables-persistent chkrootkit rkhunter rsyslog ranger htop tcpdump net-tools nmap dnsutils jq sudo
+
+echo "Установка базовых программ"
+apt install sudo mc git iptables-persistent chkrootkit rkhunter rsyslog micro htop tcpdump net-tools dnsutils jq
 
 # Включаем unattended-upgrades
 
-echo "### Включение Unattended Upgrades ###"
+echo "Включение Unattended Upgrades"
 apt update && apt install -y unattended-upgrades
 echo 'APT::Periodic::Update-Package-Lists "1";
 APT::Periodic::Unattended-Upgrade "1";' > /etc/apt/apt.conf.d/20auto-upgrades
 sed -i 's|//\s*"${distro_id}:${distro_codename}-security";|"${distro_id}:${distro_codename}-security";|' /etc/apt/apt.conf.d/50unattended-upgrades
-echo "### Автообновления включены ###"
+echo "Автообновления включены"
 
 # Включаем BBR, отключаем IPv6
                                                                           
@@ -29,13 +33,13 @@ net.ipv6.conf.default.disable_ipv6 = 1
 net.ipv6.conf.lo.disable_ipv6 = 1
 " >> /etc/sysctl.conf'
 sysctl -p
-echo "### Если вы видите ошибку, откройте /etc/sysctl.conf и измените eth0 на ваш интерфейс из ifconfig ###"
+echo "Если вы видите ошибку, откройте /etc/sysctl.conf и измените eth0 на ваш интерфейс из ifconfig"
 read -r
 
 # Включаем автоматическую перезагрузку в 4:20 утра каждый понедельник
 
 (crontab -l 2>/dev/null; echo "20 4 * * 1 /sbin/reboot") | crontab -
-echo "### Добавлена перезагрузка в 4:20 каждый понедельник ###"
+echo "Добавлена перезагрузка в 4:20 каждый понедельник"
 
 # Устанавливаем и настраиваем Fail2Ban
 
@@ -50,67 +54,67 @@ maxretry = 5
 enabled = true
 EOF
 systemctl enable fail2ban && systemctl start fail2ban
-echo "### fail2ban настроен: бан на 12 часов спустя 5 попыток за 15 минут ###"
+echo "fail2ban настроен: бан на 12 часов спустя 5 попыток за 15 минут"
 
 # Добавляем пользователя sudo
                                                                    
-read -p "### Введите имя пользователя: ###" username
+read -p "Введите имя нового пользователя sudo:" username
 adduser --gecos "" $username
 usermod -aG sudo $username
-echo "### Пользователь $username создан и добавлен в sudo ###"
+echo "Пользователь $username создан и добавлен в sudo"
 
 # Включаем логгирование sudo
                                         
-echo "### В следующем файле добавьте строку 'Defaults logfile=/var/log/sudo.log, log_input, log_output'"
-read -r -p "### Окей, я скопировал её и готов ###"
+echo "В следующем файле добавьте строку 'Defaults logfile=/var/log/sudo.log, log_input, log_output'"
+read -r -p "Окей, я скопировал её и готов"
 sudo visudo
 
 # Для AmneziaVPN self-hosted
 
-read -p "### Вы разворачиваете AmneziaVPN self-hosted? Y/n: ###" -r
+read -p "Вы разворачиваете AmneziaVPN self-hosted? Y/n:" -r
 echo
 
 if [[ $REPLY =~ ^[Yy]$ ]]; then
     echo "$username ALL=(ALL) NOPASSWD: ALL" | sudo EDITOR='tee' visudo -f /etc/sudoers.d/$username-nopasswd
-    echo "### sudo-операции для $username НЕ БУДУТ ТРЕБОВАТЬ ПАРОЛЬ ###"
+    echo "sudo-операции для $username НЕ БУДУТ ТРЕБОВАТЬ ПАРОЛЬ"
 else
-    echo "### sudo-операции для $username будут происходить как обычно ###"
+    echo "sudo-операции для $username будут происходить как обычно"
 fi
 
 # Отключаем вход через root
 
 sed -i 's/^PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config
 systemctl restart ssh
-echo "### Вход под root отключен ###"
+echo "Вход под root отключен"
 
 echo "======================================================================================="
-echo "### ВНИМАТЕЛЬНО ПРОЧТИТЕ: сейчас вам нужно открыть терминал на своём компьютере "
-echo "### И создать ssh-ключ для входа на сервер следующей командой: "
-echo "### ssh-keygen -t ed25519 -C "имяключа" -f ~/.ssh/имяключа "
-echo "### Загрузите ключ на сервер (команда для Linux): "
-echo "### ssh-copy-id -i ~/.ssh/имяключа.pub логин@IP.сервера "
-echo "### Команда для Windows: "
-echo "### cat ~/.ssh/id_ed25519.pub | ssh логин@IP.сервера" mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys" "
-echo "### Попробуйте подключиться к вашему серверу при помощи этого ключа: "
-echo "### ssh -i ~/.ssh/имяключа логин@IP.сервера "
-echo "### ТОЛЬКО ЕСЛИ ВХОД УСПЕШЕН, нажмите здесь Enter для ОТКЛЮЧЕНИЯ ВХОДА В SSH ПО ПАРОЛЮ "
+echo "ВНИМАТЕЛЬНО ПРОЧТИТЕ: сейчас вам нужно открыть терминал на своём компьютере"
+echo "И создать ssh-ключ для входа на сервер следующей командой:"
+echo "ssh-keygen -t ed25519 -C "имяключа" -f ~/.ssh/имяключа"
+echo "Загрузите ключ на сервер (команда для Linux):"
+echo "ssh-copy-id -i ~/.ssh/имяключа.pub логин@IP.сервера"
+echo "Команда для Windows:"
+echo "cat ~/.ssh/id_ed25519.pub | ssh логин@IP.сервера mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys"
+echo "Попробуйте подключиться к вашему серверу при помощи этого ключа: "
+echo "ssh -i ~/.ssh/имяключа логин@IP.сервера "
+echo "ТОЛЬКО ЕСЛИ ВХОД УСПЕШЕН, нажмите здесь Enter для ОТКЛЮЧЕНИЯ ВХОДА В SSH ПО ПАРОЛЮ "
 echo "======================================================================================="
 read -r
 grep -r PasswordAuthentication /etc/ssh -l | xargs -n 1 sed -i -e "/PasswordAuthentication /c\PasswordAuthentication no"
-echo "### Вход по паролю отключён ###"
+echo "Вход по паролю отключён"
 
 # Смена SSH-порта
 
-read -p "### Введите новый SSH порт: ###" port
+read -p "Введите новый SSH порт:" port
 sed -i "s/^#Port.*/Port $port/" /etc/ssh/sshd_config
 sed -i "s/^Port.*/Port $port/" /etc/ssh/sshd_config
 grep -q "^Port" /etc/ssh/sshd_config || echo "Port $port" >> /etc/ssh/sshd_config
 systemctl restart ssh
-echo "### SSH порт изменен на $port ###"
+echo "SSH порт изменен на $port"
 
 # Настройка iptables и блокировка IP-адресов РКН
 
-echo "### Настройка iptables ###"
+echo "Настройка iptables"
 iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -A INPUT -p tcp -m state --state NEW -m tcp --dport $port -j ACCEPT 
 iptables -A INPUT -p tcp -m tcp --dport 80 -j ACCEPT
@@ -125,13 +129,13 @@ git clone https://github.com/freemedia-tech/iptables-rugov-block.git
 cd iptables-rugov-block
 ./install.sh
 
-echo "### Настройка iptables завершена ###"
+echo "Настройка iptables завершена"
 
 # Добавляем 2 Гб подкачки для самых дешёвых VPS с 1 Гб RAM
 
 swapfile="/swapfile"
 if [ -f "$swapfile" ]; then
-    echo "### Swap файл уже существует ###"
+    echo "Swap файл уже существует"
     exit 1
 fi
 dd if=/dev/zero of=$swapfile bs=1M count=2048
@@ -139,7 +143,7 @@ chmod 600 $swapfile
 mkswap $swapfile
 swapon $swapfile
 echo "$swapfile none swap sw 0 0" >> /etc/fstab
-echo "### Создан swap файл 2GB и активирован ###"
+echo "Создан swap файл 2GB и активирован"
                            
 # Смотрим GeoIP
                 
