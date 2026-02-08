35ac9e22259aacd45f02380552e5ec54ac086f1a
vrzdrb <amhdws@protonmail.com>
2026-02-09 00:24:12 +0500

    Фиксы, оформление

--- vps_autosetup.sh
+++ vps_autosetup.sh
@@ -1,33 +1,37 @@
 #!/bin/bash
 
-
 # Проверка на root
-
 if [ "$(id -u)" != "0" ]; then
    echo "Этот скрипт должен быть запущен от root" 1>&2
    exit 1
 fi
 
-
 # Прерывание при ошибке
-
 set -e
 
+# Яркий голубой
+echo -e "\e[96m$1\e[0m"
 
-# Обновление системы
+# Яркий розовый
+echo -e "\e[38;5;213m$1\e[0m"
 
-echo "Обновление"                                                                                          
-apt update
-apt upgrade
+echo -e " \e[96m███████╗██╗   ██╗███████╗    ██╗   ██╗██████╗ ██████╗  █████╗ ████████╗███████╗
+██╔════╝╚██╗ ██╔╝██╔════╝    ██║   ██║██╔══██╗██╔══██╗██╔══██╗╚══██╔══╝██╔════╝
+███████╗ ╚████╔╝ ███████╗    ██║   ██║██████╔╝██║  ██║███████║   ██║   █████╗  
+╚════██║  ╚██╔╝  ╚════██║    ██║   ██║██╔═══╝ ██║  ██║██╔══██║   ██║   ██╔══╝  
+███████║   ██║   ███████║    ╚██████╔╝██║     ██████╔╝██║  ██║   ██║   ███████╗
+╚══════╝   ╚═╝   ╚══════╝     ╚═════╝ ╚═╝     ╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚══════╝\e[0m"
 
-# Шифрование DNS
-echo ""
-echo ""
-echo "Настройки DNS..."
-echo ""
-echo ""
+apt update && apt upgrade -y
 
-apt install systemd-resolved
+echo -e "\e[38;5;213m██████╗ ███╗   ██╗███████╗
+██╔══██╗████╗  ██║██╔════╝
+██║  ██║██╔██╗ ██║███████╗
+██║  ██║██║╚██╗██║╚════██║
+██████╔╝██║ ╚████║███████║
+╚═════╝ ╚═╝  ╚═══╝╚══════╝\e[0m"
+                          
+apt install systemd-resolved -y
 
 tee /etc/systemd/resolved.conf <<EOF
 [Resolve]
@@ -42,48 +46,33 @@ systemctl enable systemd-resolved.service
 systemctl start systemd-resolved.service
 systemctl restart systemd-resolved.service
 
-echo ""
-echo ""
-echo "Настройки DNS применены"
-echo ""
-echo ""
+echo -e "\e[96m████████╗ ██████╗  ██████╗ ██╗     ███████╗
+╚══██╔══╝██╔═══██╗██╔═══██╗██║     ██╔════╝
+   ██║   ██║   ██║██║   ██║██║     ███████╗
+   ██║   ██║   ██║██║   ██║██║     ╚════██║
+   ██║   ╚██████╔╝╚██████╔╝███████╗███████║
+   ╚═╝    ╚═════╝  ╚═════╝ ╚══════╝╚══════╝\e[0m"
 
-# Базовый набор программ
-echo ""
-echo ""
-echo "Установка базового ПО"
-echo ""
-echo ""
-apt install unattended-upgrades sudo mc ufw chkrootkit micro htop tcpdump net-tools dnsutils jq ranger iftop nethogs bmon
-echo ""
-echo ""
-echo "Установка завершена"
-echo ""
-echo ""
+apt install unattended-upgrades sudo mc ufw chkrootkit micro htop tcpdump net-tools dnsutils jq iftop nethogs bmon
 
-
-# Включаем unattended-upgrades
-echo ""
-echo ""
-echo "Включаем unattended-upgrades"
-echo ""
-echo ""
+echo -e "\e[38;5;213m██╗   ██╗███╗   ██╗ █████╗ ████████╗████████╗███████╗███╗   ██╗██████╗ ███████╗██████╗ 
+██║   ██║████╗  ██║██╔══██╗╚══██╔══╝╚══██╔══╝██╔════╝████╗  ██║██╔══██╗██╔════╝██╔══██╗
+██║   ██║██╔██╗ ██║███████║   ██║      ██║   █████╗  ██╔██╗ ██║██║  ██║█████╗  ██║  ██║
+██║   ██║██║╚██╗██║██╔══██║   ██║      ██║   ██╔══╝  ██║╚██╗██║██║  ██║██╔══╝  ██║  ██║
+╚██████╔╝██║ ╚████║██║  ██║   ██║      ██║   ███████╗██║ ╚████║██████╔╝███████╗██████╔╝
+ ╚═════╝ ╚═╝  ╚═══╝╚═╝  ╚═╝   ╚═╝      ╚═╝   ╚══════╝╚═╝  ╚═══╝╚═════╝ ╚══════╝╚═════╝\e[0m"
+                                                                                       
 echo 'Unattended-Upgrade::Mail "root";' >> /etc/apt/apt.conf.d/50unattended-upgrades
 echo unattended-upgrades unattended-upgrades/enable_auto_updates boolean true | debconf-set-selections
 dpkg-reconfigure -f noninteractive unattended-upgrades
 systemctl restart unattended-upgrades
-echo ""
-echo ""
-echo "unattended-upgrades включены"
-echo ""
-echo ""
 
-# Включаем BBR, отключаем IPv6
-echo ""
-echo ""
-echo "Включаем BBR, отключаем IPv6"
-echo ""
-echo ""
+echo -e "\e[96m██████╗ ██████╗ ██████╗     ██╗██████╗ ██╗   ██╗ ██████╗ 
+██╔══██╗██╔══██╗██╔══██╗    ██║██╔══██╗██║   ██║██╔════╝ 
+██████╔╝██████╔╝██████╔╝    ██║██████╔╝██║   ██║███████╗ 
+██╔══██╗██╔══██╗██╔══██╗    ██║██╔═══╝ ╚██╗ ██╔╝██╔═══██╗
+██████╔╝██████╔╝██║  ██║    ██║██║      ╚████╔╝ ╚██████╔╝
+╚═════╝ ╚═════╝ ╚═╝  ╚═╝    ╚═╝╚═╝       ╚═══╝   ╚═════╝\e[0m"
 
 if ! grep -q "net.core.default_qdisc = fq" /etc/sysctl.conf; then
     echo "net.core.default_qdisc = fq" >> /etc/sysctl.conf
@@ -103,91 +92,41 @@ fi
 
 sysctl -p
 
-echo ""
-echo ""
-echo "BBR включен, IPv6 отключен"
-echo ""
-echo ""
+echo -e "\e[38;5;213m██████╗ ███████╗██████╗  ██████╗  ██████╗ ████████╗
+██╔══██╗██╔════╝██╔══██╗██╔═══██╗██╔═══██╗╚══██╔══╝
+██████╔╝█████╗  ██████╔╝██║   ██║██║   ██║   ██║   
+██╔══██╗██╔══╝  ██╔══██╗██║   ██║██║   ██║   ██║   
+██║  ██║███████╗██████╔╝╚██████╔╝╚██████╔╝   ██║   
+╚═╝  ╚═╝╚══════╝╚═════╝  ╚═════╝  ╚═════╝    ╚═╝\e[0m"
 
-# Включаем автоматическую перезагрузку в 4:20 утра каждый понедельник
-echo ""
-echo ""
-echo "Включаем автоматическую перезагрузку"
-echo ""
-echo ""
+timedatectl set-timezone Europe/Moscow
+echo "20 2 * * 1 /sbin/reboot" | tee -a /var/spool/cron/crontabs/root >/dev/null
 
-(crontab -l 2>/dev/null; echo "20 4 * * 1 /sbin/reboot") | crontab -
+echo -e "\e[96m███████╗███████╗██╗  ██╗
+██╔════╝██╔════╝██║  ██║
+███████╗███████╗███████║
+╚════██║╚════██║██╔══██║
+███████║███████║██║  ██║
+╚══════╝╚══════╝╚═╝  ╚═╝\e[0m"
 
-echo ""
-echo ""
-echo "Добавлена перезагрузка в 4:20 каждый понедельник"
-echo ""
-echo ""
+=======================================================================================
+ВНИМАТЕЛЬНО ПРОЧТИТЕ: сейчас вам нужно открыть терминал на своём компьютере
+И создать ssh-ключ для входа на сервер следующей командой:"
+ssh-keygen -t ed25519 -C "имяключа" -f имяключа"
 
-# Добавляем пользователя sudo                                                  
-echo ""
-echo ""
-read -p "Введите имя нового пользователя sudo:" username
-echo ""
-echo ""
-adduser --gecos "" $username
-usermod -aG sudo $username
-echo ""
-echo ""
-echo "Пользователь $username создан и добавлен в sudo"
-echo ""
-echo ""
+Загрузите ключ на сервер (команда для Linux):
+ssh-copy-id -i имяключа.pub root@IP.сервера
 
-# Включаем логгирование sudo                            
-echo ""
-echo ""
-echo "В следующем файле добавьте строку 'Defaults logfile=/var/log/sudo.log, log_input, log_output'"
-echo ""
-echo ""
-read -r -p "Окей, я скопировал её и готов"
-sudo visudo
+Команда для Windows:
+cat id_ed25519.pub | ssh root@IP.сервера mkdir -p && cat >> ~/.ssh/authorized_keys
 
+Попробуйте подключиться к вашему серверу при помощи этого ключа:
+ssh -i имяключа root@IP.сервера
 
-# Для AmneziaVPN self-hosted
-echo ""
-echo ""
-read -p "Вы разворачиваете AmneziaVPN self-hosted? Y/n:" -r
-echo ""
-echo ""
-echo
+ТОЛЬКО ЕСЛИ ВХОД УСПЕШЕН, нажмите здесь Enter для ОТКЛЮЧЕНИЯ ВХОДА В SSH ПО ПАРОЛЮ 
+=======================================================================================
 
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-    echo "В следующем диалоговом окне вместо $username пишите root"
-else
-    echo "Ок"
-fi
-
-
-# Настройка безопасности SSH:
-echo ""
-echo ""
-echo "======================================================================================="
-echo "ВНИМАТЕЛЬНО ПРОЧТИТЕ: сейчас вам нужно открыть терминал на своём компьютере"
-echo "И создать ssh-ключ для входа на сервер следующей командой:"
-echo "ssh-keygen -t ed25519 -C "имяключа" -f имяключа"
-echo ""
-echo "Загрузите ключ на сервер (команда для Linux):"
-echo "ssh-copy-id -i имяключа.pub $username@IP.сервера"
-echo ""
-echo "Команда для Windows:"
-echo "cat id_ed25519.pub | ssh $username@IP.сервера mkdir -p && cat >> ~/.ssh/authorized_keys"
-echo ""
-echo "Попробуйте подключиться к вашему серверу при помощи этого ключа: "
-echo "ssh -i имяключа $username@IP.сервера "
-echo ""
-echo "ТОЛЬКО ЕСЛИ ВХОД УСПЕШЕН, нажмите здесь Enter для ОТКЛЮЧЕНИЯ ВХОДА В SSH ПО ПАРОЛЮ "
-echo "======================================================================================="
 read -r
-echo ""
-echo ""
-echo "Применяем настройки безопасности SSH"
-echo ""
-echo ""
 
 SSHD_CONFIG="/etc/ssh/sshd_config"
 SSH_CONFIG="/etc/ssh/ssh_config"
@@ -261,15 +200,7 @@ KerberosAuthentication no
 EOF
 
 echo ""
-echo ""
-echo "Настройки безопасности SSH применены"
-echo ""
-echo ""
-echo "Смена SSH-порта"
-echo ""
-echo ""
-read -p "Введите новый SSH порт:" port
-echo ""
+read -p "\e[96mВведите новый SSH порт:\e[0m" port
 echo ""
 
 chmod 600 ~/.ssh/authorized_keys
@@ -278,11 +209,6 @@ sed -i "s/^#Port.*/Port $port/" /etc/ssh/sshd_config.d/99-security-settings.conf
 sed -i "s/^Port.*/Port $port/" /etc/ssh/sshd_config.d/99-security-settings.conf
 grep -q "^Port" /etc/ssh/sshd_config.d/99-security-settings.conf || echo "Port $port" >> /etc/ssh/sshd_config.d/99-security-settings.conf
 
-echo ""
-echo ""
-echo "Перезапуск службы SSH..."
-echo ""
-echo ""
 if systemctl restart sshd; then
     echo "Служба SSH перезапущена через sshd"
 else
@@ -290,18 +216,16 @@ else
     echo "Служба SSH перезапущена через ssh"
 fi
 
-echo ""
-echo ""
-echo "SSH порт изменен на $port"
-echo ""
-echo ""
+print_bright_cyan "SSH порт изменен на $port"
 
-# Настройка ufw и блокировка IP-адресов РКН
-echo ""
-echo ""
-echo "Базовая настройка ufw"
-echo ""
-echo ""
+echo -e "\e[96m██╗   ██╗███████╗██╗    ██╗
+██║   ██║██╔════╝██║    ██║
+██║   ██║█████╗  ██║ █╗ ██║
+██║   ██║██╔══╝  ██║███╗██║
+╚██████╔╝██║     ╚███╔███╔╝
+ ╚═════╝ ╚═╝      ╚══╝╚══╝ 
+                           \e[0m"
+                           
 
 IP4_REGEX="^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$"
 IP4=$(ip route get 8.8.8.8 2>/dev/null | grep -Po -- 'src \K\S*')
@@ -321,38 +245,6 @@ ufw limit $port/tcp comment 'SSH'
 ufw allow 443/tcp comment 'WEB'
 ufw insert 1 deny from "$BLOCK_ZONE_IP"
 ufw --force enable
-
-echo ""
-echo ""
-echo "Базовая настройка ufw прошла успешно"
-echo ""
-echo ""
-echo "Блокировка IP-адресов РКН"
-echo ""
-echo ""
-cat > add_blacklist.sh << 'EOF'
-wget -O blacklist.txt https://raw.githubusercontent.com/C24Be/AS_Network_List/main/blacklists/blacklist.txt
-if [[ ! -f blacklist.txt ]]; then
-   echo "Файл blacklist.txt не найден!"
-   exit 1
-fi
-while read -r subnet; do
-    ufw deny from "$subnet"
-done < blacklist.txt
-ufw status
-EOF
-chmod +x add_blacklist.sh
-echo "Запускаем add_blacklist.sh..."
-./add_blacklist.sh
-if ! crontab -l 2>/dev/null | grep -q "add_blacklist.sh"; then
-    (crontab -l 2>/dev/null; echo "0 5 * * * $(pwd)/add_blacklist.sh") | crontab -
-    echo "Задание добавлено в crontab для ежедневного выполнения в 5:00 утра"
-else
-    echo "Задание уже существует в crontab"
-fi
-
-echo "Настройка ufw под РКН завершена!"
-
 
 # Смотрим GeoIP 
 wget -O ipregion.sh https://ipregion.vrnt.xyz
@@ -375,12 +267,13 @@ chmod +x vps-audit.sh
 sudo ./vps-audit.sh
 read -r
 
-# Добавляем 1 Гб подкачки для самых дешёвых VPS с 1 Гб RAM
-echo ""
-echo ""
-echo "Добавляем 1Гб подкачки"
-echo ""
-echo ""
+echo -e "\e[38;5;213m███████╗██╗    ██╗ █████╗ ██████╗ 
+██╔════╝██║    ██║██╔══██╗██╔══██╗
+███████╗██║ █╗ ██║███████║██████╔╝
+╚════██║██║███╗██║██╔══██║██╔═══╝ 
+███████║╚███╔███╔╝██║  ██║██║     
+╚══════╝ ╚══╝╚══╝ ╚═╝  ╚═╝╚═╝     
+                                  \e[0m"
 
 swapfile="/swapfile"
 if [ -f "$swapfile" ]; then
@@ -394,7 +287,5 @@ swapon $swapfile
 echo "$swapfile none swap sw 0 0" >> /etc/fstab
 
 echo ""
-echo ""
 echo "1Гб подкачки создан и активирован"
-echo ""
 echo ""
